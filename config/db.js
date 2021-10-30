48f1aa00e179208a76298eb8cd7d7790a8e7d9d3
Daniel-M-Ayoub <90784832+Daniel-M-Ayoub@users.noreply.github.com>
2021-10-30 15:27:49 -0400

    Midterm working version

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // Point to sample data in MongoDB atlas
+    "AtlasDB": "mongodb+srv://daniel:oyBwhof6kKMXyKJq@mongodbserver.zgk3j.mongodb.net/midterm-009?retryWrites=true&w=majority"
+}
